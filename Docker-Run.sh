e2266114389c19a869c6f75c5c767f1598cf62ab
MD SUMON H. <etytii70+20@gmail.com>
2024-08-30 15:17:26 -0400

    Update and rename Docker Run to Docker-Run.sh

--- Docker-Run.sh
+++ Docker-Run.sh
@@ -1,8 +1,8 @@
-sudo docker run -d -t --name usename# conteinar-name
-sudo docker ps #@ -a
-sudo docker exec -it myubontu bash
+# sudo docker run -d -t --name usename# conteinar-name
+#sudo docker ps #@ -a
+#sudo docker exec -it myubontu bash
+# fast @ sudo pacman -U ./docker-desktop-<arch>.pkg.tar.zst
 sudo pacman -S gnome-terminal
-sudo pacman -U ./docker-desktop-<arch>.pkg.tar.zst
 systemctl --user start docker-desktop
 systemctl --user enable docker-desktop
 sudo systemctl enable --now docker.service
